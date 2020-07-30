324d40c72f83e9986422dc9fb8b27de9ff817600
RichardPitts1 <Richardepitts@gmail.com>
2020-07-30 07:36:28 -0500

    2

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -31,7 +33,7 @@ router.post('/', [
     let user = await User.findOne({ email: email });
 
     if(user) {
-        res.status(400).json({ errors: [ { msg: 'User already exists' }] });
+        return res.status(400).json({ errors: [ { msg: 'User already exists' }] });
     }
 
     const avatar = gravatar.url(email, {
@@ -54,17 +56,24 @@ router.post('/', [
 
     await user.save();
 
-    // Return jsonwebtoken
-        res.send('User registered');
-    } catch {
+      const payload = {
+          user: {
+              id: user.id
+          }
+      }
+
+      jwt.sign(
+          payload, 
+          config.get('jwtSecret'),
+          { expiresIn: 36000 },
+          (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+          });
+    } catch (err) {
         console.error(err.message);
         res.status(500).send('Ser')
-    }
-
-
-
-
-    
+    } 
 });
 
 module.exports = router;
