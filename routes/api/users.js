f92fd498392d82387568f9d3d1fb6c14bdc4519b
RichardPitts1 <Richardepitts@gmail.com>
2020-07-30 06:38:19 -0500

    2

--- routes/api/users.js
+++ routes/api/users.js
@@ -26,7 +26,11 @@ router.post('/', [
     const { name, email, password } = req.body;
 
     try {
-            // See if user exists
+    let user = await User.findOne({ email: email });
+
+    if(user) {
+        res.status(400).json({ errors: [ { msg: 'User already exists' }] });
+    }
 
     // Get users gravatar
 
@@ -36,6 +40,7 @@ router.post('/', [
         res.send('User route');
     } catch {
         console.error(err.message);
+        res.status(500).send('Ser')
     }
 
 
