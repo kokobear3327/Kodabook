5d149295c4b8411ea2453722e368ccb829e169ee
RichardPitts1 <Richardepitts@gmail.com>
2020-07-30 19:12:40 -0500

    2

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -324,12 +324,12 @@ router.get("/github/:username", (req, res) => {
         "githubClientId"
       )}&client_secret=${config.get("githubSecret")}`,
       method: "GET",
-      headers: { "user-agent": "node.js" },
+      headers: { "user-agent": "node.js" }
     };
     request(options, (error, response, body) => {
       if (error) console.error(error);
       if (response.statusCode != 200) {
-        res.status(404).json({ msg: "No Github Profile found" });
+        return res.status(404).json({ msg: "No Github Profile found" });
       }
       res.json(JSON.parse(body));
     });
